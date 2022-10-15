12a0ec482c6e28a022c99196e705c850b332c139
NimraAbid05 <nimram214@gmail.com>
2022-10-15 08:49:30 -0700

    variable

---
+++ variables.dart
@@ -0,0 +1,10 @@
+void main(){
+  
+  String name="Nimra";  //here name is a variable that store value "Nimra"
+  print(name);
+
+  int age=20;  //age is another variable
+  print("I'm $age years old!");
+
+  
+}
